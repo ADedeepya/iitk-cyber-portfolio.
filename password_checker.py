b709e5c411acd3751070188dec0aba2ce6a84f1f
ADedeepya <atikimsettidedeepya@gmail.com>
2026-06-25 09:15:44 +0530

    Update password_checker.py

--- password_checker.py
+++ password_checker.py
@@ -1,1 +1,16 @@
-
+def passwordChecker(password):
+    if len(password) < 5:
+        return False
+        
+    caps = any(c.isupper() for c in password)
+    nums = any(c.isdigit() for c in password)
+    symb = any(c in '!@#$%^&' for c in password)
+    
+    if not caps:
+        return False
+    elif not nums:
+        return False
+    elif not symb:
+        return False
+    else:
+        return True
